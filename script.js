3926dc8f313b3926db001d083717d61e51cc3a8a
santosrenan21-arch <santos.renan21@escola.pr.gov.br>
2026-09-09 17:22:25 -0300

    atualizei o codigo

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+const botao = document.querySelector("#botaoMensagem");
+cosnt mensagem = document.querySelector('#mensagemInterativa');
+botao.addEventListener("click, mostrarMensagem");
+function mostrarMensagem() {
+    mensagem.textContent =
+      "Você acabou de criar uma interação JavaScript";
+}
