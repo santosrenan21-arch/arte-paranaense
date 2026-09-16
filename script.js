d3a3620414dafdf5a3ea648191ebb49838c46f79
santosrenan21-arch <santos.renan21@escola.pr.gov.br>
2026-09-16 17:54:25 -0300

    alterei o codigo

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const botao = document.querySelector("#botaoMensagem");
-const mensagem = document.querySelector("#mensagemInterativa");
-botao.addEventListener("click", mostrarMensagem);
-function mostrarMensagem() {
-    mensagem.textContent =
-      "Você acabou de criar uma interação JavaScript";
+const botoesCurtir = document.querySelectorAll(".curtir");
+botoesCurtir.forEach(function(botaoCurtir){
+    let curtiu = false;
+    botaoCurtir.addEventListener("click", curtir);
+    function curtir(){
+        const contador = botaoCurtir.querySelector("span");
+        if(curtiu === false){
+            contador.textContent++;
+            curtiu = true;}
+            else{
+                contador.textContent--;
+                curtiu = false;
+            }
 }
+});
